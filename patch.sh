7b6b93e4e565b54b8e59d660ecd3e4654bd92263
foss-mc <69294560+foss-mc@users.noreply.github.com>
2021-06-26 03:33:29 +0000

    add functionality

---
+++ patch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+workdir=`mktemp -d`
+clean_up () {
+    cd
+    rm -fr "$workdir"
+}
+trap clean_up EXIT
+bindir="$(cd `dirname "$0"` && pwd)"
+fernflower="$bindir/fernflower.jar"
+apatch="$bindir/a.patch"
+
+jar="$workdir/file.jar"
+cp "$1" "$jar"
+userdir=`pwd`
+
+classes="$workdir/classes"
+decomp="$workdir/decomp"
+mkdir -p "$classes"
+mkdir -p "$decomp"
+cd "$classes"
+unzip "$jar" com/vk/api/sdk/httpclient/HttpTransportClient.class
+java -jar "$fernflower" "$classes" "$decomp"
+
+cd "$decomp"
+patch -p1 --no-backup-if-mismatch < "$apatch"
+javac -cp "$jar" com/vk/api/sdk/httpclient/HttpTransportClient.java
+rm com/vk/api/sdk/httpclient/HttpTransportClient.java
+zip "$jar" com
+
+cd "$userdir"
+mv "$jar" "$2"
